57306b31e91855631e0e30e5be878934f2167267
janxavierv <37735268+janxavierv@users.noreply.github.com>
2021-03-11 22:01:11 +1100

    added completed part one of the project

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-const game = new Game();
+
 // const logPhrase = (phrase) => {
 //     console.log(`Phrase - phrase: `, phrase.phrase, "length", phrase.phrase.length);
 // };
 // logPhrase(game.getRandomPhrase());
 // game.getRandomPhrase().addPhraseToDisplay();
 
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game
+let startButton = document.querySelector("#btn__reset")
+startButton.addEventListener("click", () => {
+    game = new Game();
+    game.startGame();
+})
